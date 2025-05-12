c62e0feb55fe83982bfa48701288b729b8c9d587
NAGARJUNAGK <gknagarjuna2002@gmail.com>
2025-05-12 21:07:38 +0530

    completed session cookie flash

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,11 @@ const app = express();
 const user = require("./routes/user.js");
 const posts = require("./routes/posts.js");
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOptions = {
   secret: "mysupersecretstring",
@@ -11,16 +16,28 @@ const sessionOptions = {
 };
 
 app.use(session(sessionOptions));
+app.use(flash());
 
-app.get("/register",(req,res)=>{
-  let{name="annonyms"} = req.query;
-  console.log(req.session);
-  res.send(`hi ${name}`);
-})
+app.use((req, res, next) => {
+  res.locals.successMsg = req.flash("success");
+  res.locals.errorMsg = req.flash("error");
+  next();
+});
 
-app.get("/hello",(req,res)=>{
-  res.send(`hello`);
-})
+app.get("/register", (req, res) => {
+  let { name = "annonyms" } = req.query;
+  req.session.name = name;
+  if (name === "annonyms") {
+    req.flash("error", "user not registered");
+  } else {
+    req.flash("success", "user registered successfully");
+  }
+  res.redirect(`/hello`);
+});
+
+app.get("/hello", (req, res) => {
+  res.render("page.ejs", { name: req.session.name });
+});
 
 // app.get("/test", (req, res) => {
 //   res.send("test successful");
