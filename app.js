20aacd26a2e4bfad329e631173956f19d9d8f7fe
NAGARJUNAGK <gknagarjuna2002@gmail.com>
2025-05-18 20:06:10 +0530

    passport fundamentals

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const path = require("path");
 const methodOverride = require("method-override"); //converting request
 const ejsMate = require("ejs-mate"); //creating layout
 const ExpressError = require("./utils/ExpressError.js"); //generating errors
-const session = require("express-session");
-const flash = require("connect-flash");
+const session = require("express-session");//for statefulness
+const flash = require("connect-flash");//generates alert messages
+const passport = require("passport");//for authentication and authorization
+const LocalStrategy = require("passport-local");//username password autentication
+const User = require("./models/user.js");
 
+//routes
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 
+//middlewares
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +23,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
+//declearing session options
 const sessionOptions = {
   secret : "mysupersecretstring",
   resave : false,
@@ -29,15 +35,27 @@ const sessionOptions = {
   }
 };
 
+//creating session and flash alearts
 app.use(session(sessionOptions));
 app.use(flash());
 
+//initializing passport
+app.use(passport.initialize());
+app.use(passport.session());//for statefulness of the routes
+passport.use(new LocalStrategy(User.authenticate()));//static authenticate method
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// flash middleware for redirecting flash to any route without rendering
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 })
 
+
+// mongoose connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -56,7 +74,17 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
+app.get("/demouser",async(req,res)=>{
+  let fakeUser = new User({
+    email:"student@gmail.com",
+    username:"delta-student"
+  });
+  let registeredUser = await User.register(fakeUser,"helloworld");
+  console.log(registeredUser);
+  res.send(registeredUser); 
+});
 
+// getting routes from other sources
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 
@@ -74,6 +102,7 @@ app.use("/listings/:id/reviews",reviewRouter);
 //   res.send("successful testing");
 // })
 
+// Error if there is no route present
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not Found"));
 });
